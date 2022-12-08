9b90549826eba19c9d05a8a32c7c50fc6acd28d9
Andrew Karam Saber Saad <androw.20371160@eng.aun.edu.eg>
2022-12-08 22:26:15 +0200

    Add files via upload

--- bank_system.cpp
+++ bank_system.cpp
@@ -1,184 +1,211 @@
 #include<iostream>
+#include<string.h>
+#define MAX 30
 using namespace std ;
-struct cusdata{
-char firstname [10] ;
-char lastname [10] ;
-int accountnumber ;
-int totalbalance ;
-
+struct node {
+int acc_num ,rec ;
+char  name[MAX] ;
+node * left ;
+node * right ;
 };
-struct link {
-cusdata customer;
-link* next ;
-};
-class bank_system {
-private:
-    link *first ;
-public:
-bank_system (){
-first = NULL ;}
-void additem_first() {
-    link *put = new link ;
-    cout<<" \n enter first name :" ;
-    cin>> put->customer.firstname;
-    cout<<" \n enter last name ";
-    cin>>put->customer.lastname ;
-    cout<<"\n enter the account number ";
-    cin>>put->customer.accountnumber ;
-    cout<<"\n enter total balance ";
-    cin>>put->customer.totalbalance ;
-    put->next = first ;
-    first = put ;
+node * get_node(int acc_num , int rec ,char * name  ){
+node * root = new node ;
+
+root->acc_num = acc_num ;
+root->rec = rec ;
+strcpy(root->name ,name) ;
+root->left = NULL ;
+root->right = NULL ;
+return root ;
 }
-void del_link (int  x ){
-link*del ;
-del = first ;
-if (del == NULL){
-    cout<<"the list is empty ";
-    return ;}
-if (del ->customer.accountnumber==x ){
-    first  = del->next ;
-    return ;}
-if (del ->next == NULL){
-    cout<<"not deleted ";
-    return ;
-
+node * insert_node(int acc_num,int rec , char * name , node * root ){
+if(root ==NULL){
+    root = get_node(acc_num,rec,name);
+    return root ;
 }
-while (del ->next!= NULL ){
-    if(del->next->customer.accountnumber== x ){
-        del->next = del->next->next ;
-        return ;
-        }
-del = del->next ;
-
-
-
+else if (acc_num ==root->acc_num){
+    cout<<"\n account number "<<acc_num<<" already in use \n";
+    return root;
 }
-cout<<"not deleted \n  ";
-
-
+else if (acc_num >root->acc_num){
+root->right =insert_node(acc_num,rec,name,root->right) ;
 }
 
-void update_rec( ){
-link * update = new link ;
-update = first ;
-int x ;
-cout<<"enter the account number \n  ";
-cin>>x ;
-if(update==NULL ){
-cout<<"the bank is empty ";
-return ;
-
-}
-if(update->customer.accountnumber ==x ){
-cout<<" \n enter your new record ";
-cin>>update->customer.totalbalance ;
-return ;
+else if (acc_num <root->acc_num){
+  root->left = insert_node(acc_num,rec,name,root->left) ;
 }
 
-while(update->next!= NULL){
-if(update->next->customer.accountnumber ==x ){
-cout<<" \n enter your new record ";
-cin>>update->next->customer.totalbalance ;
-return ;
 }
-update = update->next ;
+node * find_max(node * root ){
+if(root ==NULL){
+    return NULL ;
+
 }
-cout<<"the account does't exist ";
+else if (root ->right==NULL){
+    return root ;
 }
-void search_acc(){
-int n ;
-link *current = new link ;
-current = first ;
+return find_max(root->right) ;
+}
+node * del_node(int acc_num,node * root ){
+if(root==NULL){
+        cout<<"system is empty ";
+    return root ;
+}
+else if (acc_num >root->acc_num){
+    root->right = del_node(acc_num,root->right) ;
+}
+
+else if (acc_num <root->acc_num){
+    root->left = del_node(acc_num,root->left) ;
+}
+else {
+    if(root->left ==NULL && root->right ==NULL){
+        delete[]root ;
+        root =NULL ;
+    }
+    else if (root ->left == NULL){
+        node * temp = root ;
+        root = root->right ;
+        delete[]temp ;
+        temp = NULL ;
+    }
+    else if (root ->right == NULL){
+        node * temp = root ;
+        root = root->left ;
+        delete[]temp ;
+        temp = NULL ;
+    }
+    else {
+        node * temp = find_max(root->left) ;
+        root->acc_num = temp->acc_num ;
+        root->rec= temp ->rec ;
+        strcpy(root->name,temp->name) ;
+        del_node(temp->acc_num,root->left) ;
+    }
+    return root ;
+}
+}
+node * search_node(int n , node * root ){
+if(root ==NULL){
+    return root ;
+}
+else if (root ->acc_num ==n){
+cout<<"\n Account number : "<<root->acc_num;
+cout<<endl<<"Full-Name : " <<root->name;
+cout<<endl<<"Record : "<<root->rec<<endl;
+return root;
+}
+else if (n>root->acc_num){
+    return search_node(n,root->right) ;
+}
+else {
+    return search_node(n,root->left) ;
+}
+}
+
+node *del_all_nodes(node * root ){
+if(root ==NULL){
+    return NULL;
+}
+del_all_nodes(root->left);
+root = del_node(root->acc_num,root);
+del_all_nodes(root->right);
+
+}
+
+void show_nodes(node * root ){
+if(root ==NULL){
+    return;
+}
+show_nodes(root->left);
+cout<<"\n Account number : "<<root->acc_num;
+cout<<endl<<"Full-Name : " <<root->name;
+cout<<endl<<"Record : "<<root->rec<<endl;
+show_nodes(root->right) ;
+}
+void update_account(node ** root){
+int choice , old_num ;
+node * view = NULL ;
+cout<<"\n specify what to update \n ";
+cout<<"\n1---name\n2---record\n3---return to main menu\n";
+cin>>choice ;
+cout<<"\n enter the account number you want to update " ;
+cin>>old_num;
+view =search_node(old_num,*root) ;
+switch(choice){
+
+case 1 : {
+cout<<"\n enter the new name";
+cin.getline(view->name,MAX,'&');
+}break ;
+case 2 :
+{
+cout<<"\n enter the new record ";
+cin>>view->rec ;
+}break ;
+case 3 : break ;
+}
+
+
+
+}
+
+node * enter_acc_via_user(node * root ){
+int acc_num,rec ;
+char name[MAX] ;
 cout<<"\n enter the account number ";
-cin>>n ;
-if(current == NULL){
-    cout<<"the list is empty \n ";
-    return ;
-}
-if(current->customer.accountnumber == n){
-    cout<<current->customer.firstname<<endl ;
-    cout<<current->customer.lastname <<endl ;
-    cout<<current->customer.accountnumber <<endl ;
-    cout<<current->customer.totalbalance <<endl ;
-    return ;
+cin>>acc_num ;
+cout<<endl<<"enter the name " ;
 
-
+cin.getline(name,MAX,'&');
+cout<<endl<<"enter the record ";
+cin>>rec;
+return insert_node(acc_num,rec,name,root);
 }
-while(current->next!= NULL){
-        if(current->next->customer.accountnumber == n ){
-    cout<<current->next->customer.firstname<<endl ;
-    cout<<current->next->customer.lastname <<endl ;
-    cout<<current->next->customer.accountnumber <<endl ;
-    cout<<current->next->customer.totalbalance <<endl ;
-    return ;
-}
-    current = current->next ;
+void interface(node * root ){
 
-}
-cout<<"The account doesn't exist or deleted ";
-
-}
-void exitst_or(){
-    int n ;
-link *current = new link ;
-current = first ;
-cout<<"\n enter the account number ";
-cin>>n ;
-if(current ==NULL){
-    cout<<"the list is empty ";
-    return ;
-
-}
-if(current->customer.accountnumber == n){
-cout<<"\n the account exist ";
-return ;
-}
-while(current->next!= NULL){
-        if(current->next->customer.accountnumber == n ){
-                cout<<"\n The account exist ";
-                return;
-        }
-         current = current->next ;
-
-
-}
-
-cout<<"the account doesn't exist ";
-}
-};
-
-int main (){
-bank_system b1 ;
+cout<<"\n---BANK-INFORMATION-SYSTEM----";
+while(1){
 int n ;
-char ch ;
-cout<<"***Acount Information System***"<<endl;
-cout<<"Select one option below"<<endl;
-cout<<"1-->Add record to the system"<<endl;
-cout<<"2-->Show record from the system"<<endl;
-cout<<"3-->Search record from the system"<<endl;
-cout<<"4-->Update record from the system"<<endl;
-cout<<"5-->Delete record from the system"<<endl;
-cout<<"6-->Quit the system"<<endl;
-do{
-cout<<"Enter your choice:\n";
+
+cout<<endl<<"1---Insert an account\n2---Search for an account\n3---Delete an account";
+cout<<endl<<"4---show all accounts (that's just for fun :) )\n6---Delete every account\n7---quit";
 cin>>n ;
 switch(n){
-case 1 : b1.additem_first(); break ;
-case 2 : b1.search_acc(); break ;
-case 3 : b1.exitst_or(); break ;
-case 4 : b1.update_rec() ; break ;
-case 5 : int x ; cout<<"enter the acc num you want to del \n ";cin>>x ;b1.del_link(x); break;
-case 6 : exit(0);
-default : cout<<"wrong inputs "; exit(0);
+case 1 :{
+root =enter_acc_via_user(root);
+}break ;
+case 2 :{
+int n  ;
+cout<<endl<<"enter the account number ";
+cin>>n ;
+node * view =NULL ;
+view = search_node(n,root);
+delete[]view ;
+view = NULL ;
+}break ;
+case 3 :{
+int n ;
+cout<<"\n enter the account number " ;
+cin>>n ;
+node * del= NULL ;
+del = del_node(n,root) ;
+delete[]del ;
+del = NULL;
+}break ;
+case 4 :show_nodes(root) ; break ;
+case 5 :update_account(&root) ;break;
+case 6: root = del_all_nodes(root); break ;
+case 7 : cout<<"\n Software was made possible by Andrew karam :) " ;system("pause"); exit(0);
 
 }
-cout<<"\n do you want another service (y /n ) ";
-cin>>ch ;
-
-}while(ch != 'n'  );
-
-    return 0 ;
 }
+}
+int main (){
 
-
+node * root = NULL ;
+interface(root) ;
+del_all_nodes(root) ;
+root = NULL;
+return 0 ;
+}
